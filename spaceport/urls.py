011f8650ea8d8adfeb816505c29f8686fcee9bba
MojosBeans100 <lucy.colley@btinternet.com>
2021-11-06 20:29:11 +0000

    etc

--- spaceport/urls.py
+++ spaceport/urls.py
@@ -4,12 +4,12 @@ from . import views
 #from . models import PipelineResults
 
 urlpatterns = [
-    path('base.html', views.homepage, name='home'),
+    path('', views.homepage, name='home'),
     path('create_pipeline2.html', views.create_pipeline2),
-    path('create_pipeline.html', views.create_pipeline),
-    path('<id>', views.detail_view, name='detail_view'),
+    path('create_pipeline.html', views.create_pipeline, name='create_pipeline'),
+    path('list_pipelines.html', views.lists, name='lists'),
     path('post_detail/<int:id>', views.detail_view, name='post_detail'),
     path('delete_pipeline/<int:id>', views.delete_pipeline, name='delete_pipeline'),
-    path('list_pipelines.html', views.lists),
-    #path('try_api.html',views.try_api)
+    #path('<id>', views.detail_view, name='detail_view'),
+    path('try_api.html', views.try_api)
 ]
